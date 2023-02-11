4dc1dc316d82d42c23b1e04bd597fca566231207
AnastasovM <marioanastasov5@gmail.com>
2023-02-11 09:43:29 +0200

    Test commit

--- WebShopDemo.gRPC/Services/ProductGrpcService.cs
+++ WebShopDemo.gRPC/Services/ProductGrpcService.cs
@@ -13,8 +13,6 @@ namespace WebShopDemo.gRPC.Services
             productService = _productService;
         }
 
-        public int DaysMissing { get; set; }
-
         public override async Task<ProductList> GetAll(Empty request, ServerCallContext context)
         {
             ProductList result = new ProductList();
